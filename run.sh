da2ed7f9f60fdaf50caf5adae939fb544ba7929c
Yurii Momotenko <momotenko.yuriy@gmail.com>
2024-03-04 19:27:18 +0100

    Build docker for linux too, update version

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 docker-compose up -d
 
-docker pull redcuckoo/merkle-tree-verifier-server:v0.2.0
+docker pull redcuckoo/merkle-tree-verifier-server:v0.3.0
 
-docker run --network merkle-tree-verifier_default -it redcuckoo/merkle-tree-verifier-client:v0.2.0
+docker run --network merkle-tree-verifier_default -it redcuckoo/merkle-tree-verifier-client:v0.3.0
 
 docker-compose down -v
